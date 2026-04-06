4a1cb88d6748994a635db96ace8955d5c0e0ccf9
CarlosEANascimento <152738170+CarlosEANascimento@users.noreply.github.com>
2026-04-05 22:09:21 -0300

    add: script com comandos de views

---
+++ database/createviewscr.sql
@@ -0,0 +1,48 @@
+CREATE OR REPLACE VIEW privado.perfil_usuario AS
+SELECT
+	usuario.id,
+	usuario.nome,
+	usuario.nome_usuario,
+	usuario.status,
+	usuario.criado_em,
+
+	identidade_visual.img_perfil,
+	identidade_visual.img_banner,
+
+	-- qtd que segue o usuário -> u = seguido
+	COUNT (DISTINCT seguido.seguido) AS seguidores,
+	-- qtd que o usuário sege -> u = segue
+	COUNT (DISTINCT segue.seguidor) AS segue,
+	COALESCE(karma.total, 0) AS karma
+
+	FROM privado.usuario AS usuario
+	JOIN privado.identidade_visual AS identidade_visual
+		ON identidade_visual.id = usuario.identidade_visual
+
+	LEFT JOIN privado.seguir_usuario AS seguido
+		ON seguido.seguido = usuario.id
+
+	LEFT JOIN privado.seguir_usuario AS segue
+		ON segue.seguidor = usuario.id
+
+	LEFT JOIN (
+		SELECT conteudo.criador, SUM(avaliacao.avaliacao) AS total
+		FROM privado.conteudo AS conteudo
+		JOIN privado.avaliacao AS avaliacao
+			ON avaliacao.conteudo = conteudo.id
+		
+		GROUP BY conteudo.criador
+	) AS karma ON karma.criador = usuario.id
+
+	WHERE usuario.excluido_em IS NULL
+
+	GROUP BY
+		usuario.id,
+		usuario.nome,
+		usuario.nome_usuario,
+		usuario.status,
+		usuario.criado_em,
+		identidade_visual.img_perfil,
+		identidade_visual.img_banner,
+		karma.total;
+
