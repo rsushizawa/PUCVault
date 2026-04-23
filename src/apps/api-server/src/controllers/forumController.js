7fd8d09ef789381b3ab04ae83c77a93aa1010093
Daniel Wu <danielwu288@gmail.com>
2026-04-23 10:07:16 -0300

    terminado módulo comentários (não testado) e configurado cors

--- src/apps/api-server/src/controllers/forumController.js
+++ src/apps/api-server/src/controllers/forumController.js
@@ -9,7 +9,21 @@ const forumSchema = z.object({
 
 exports.print = async (req, res) => {
   try {
-    await forumService.printForums();
+    const res = await forumService.printForums();
+
+    console.table(res.rows);
+  } catch (error) {
+    console.log('internal server error: ', error.message);
+  }
+};
+
+exports.files = async (req, res) => {
+
+  const { forum_id, page_num } = req.params;
+  try {
+    const res = await forumService.listForumFiles(forum_id, page_num);
+
+    console.table(res.rows);
   } catch (error) {
     console.log("internal server error: ", error.message);
   }
