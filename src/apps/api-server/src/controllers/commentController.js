ebb5318639ca349cb0b82fd27772e6b29ce622f7
Daniel Wu <danielwu288@gmail.com>
2026-04-23 10:07:16 -0300

    terminado módulo comentários (não testado) e configurado cors

---
+++ src/apps/api-server/src/controllers/commentController.js
@@ -0,0 +1,36 @@
+const commentService = require('../services/commentServices');
+const { z } = require('zod');
+
+const commentSchema = z.object({
+  content: z.string()
+});
+
+exports.createComment = async (req, res) => {
+  const validation = commentSchema.safeParse(req.body);
+  if (!validation.success) {
+    return res.status(400).json({ error: "invalid data", detail: validation.error.format() });
+  }
+  const { content } = validation.data;
+  const user_id = req.user.id;
+  const father_id = req.params;
+
+  try {
+    await commentService.createComment(content, user_id, father_id);
+    res.status(200).json({ message: "success" });
+
+  } catch (error) {
+    res.status(500).json({ error: "internal server error" });
+  }
+
+};
+
+exports.listComments = async (req, res) => {
+  const { post_id } = req.params;
+  try {
+    await commentService.listComments(post_id);
+    res.status(200).json({ message: "success" });
+
+  } catch (error) {
+    res.status(500).json({ error: "internal server error" });
+  }
+};
